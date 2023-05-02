b7db1b95b8f587164c8e670ba199e73fd4a7ea5a
Zhayida Haishan <zahideqeyser01@gmail.com>
2023-05-02 11:09:02 -0700

    finished project

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 let index =0;
 
 function changeColors(){
-    let colors = ['red', 'blue', 'yellow', 'pink', 'green', 'purple', 'orange']
+    let colors = ['red', 'blue', 'yellow', 'pink', 'green', 'purple', 'orange', 'white', 'grey']
 
     document.getElementsByTagName('body')[0].
     style.background = colors[index++];
 
     if(index > colors.length - 1)
+    index = 0;
 }
